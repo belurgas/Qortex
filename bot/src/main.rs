944de864d7114ce0e66e6e60533ff9266a97cbe7
Belurgas <70036882+belurgas@users.noreply.github.com>
2025-05-28 22:00:21 +0300

    Some sample

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,11 +1,77 @@
 use monitor::{print_all, say_hello};
+use dotenvy::dotenv;
+use teloxide::{adaptors::{throttle::Limits, Throttle}, dispatching::dialogue::InMemStorage, dptree::case, prelude::*, utils::command::BotCommands};
+use std::env;
+
+#[derive(Clone, Default, Debug)]
+pub enum State {
+    #[default]
+    OnWaiting,
+    Send,
+}
+
+#[derive(BotCommands, Clone)]
+#[command(rename_rule = "snake_case", description = "These commands are supported:")]
+enum Command {
+    #[command(description = "display this text.")]
+    Help,
+    #[command(description = "Запускает бота")]
+    Start,
+    #[command(description = "Отправляет сообщение админу",)]
+    SendMessage(String),
+}
 
 
+type MyDialogue = Dialogue<State, InMemStorage<State>>;
+type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
+type MyBot = Throttle<Bot>;
 
-fn main() {
-    let a = monitor::add(10, 15);
-    let output = format!("Число: {}", a);
-    println!("{}", output);
-    say_hello!();
-    print_all!(1, "текст", false);
+async fn answer(bot: MyBot, msg: Message, cmd: Command, fsm: MyDialogue) -> HandlerResult {
+    match cmd {
+        Command::Help => bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?,
+        Command::Start => {
+            println!("Состояние: {:?}", fsm.get().await.unwrap());
+            bot.send_message(msg.chat.id, format!("Запустили")).await?
+        }
+        Command::SendMessage(message) => {
+            bot.send_message(msg.chat.id, format!("Сообщение: {} отправлено", message))
+                .await?
+        }
+    };
+
+    Ok(())
+}
+
+async fn send(bot: MyBot, dialogue: MyDialogue, msg: Message) -> HandlerResult {
+    bot.send_message(msg.chat.id, "Let's start! Send piska?").await?;
+    // dialogue.update(State::ReceiveFullName).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let token = env::var("TOKEN").expect("Ошибка при получение токена из .env");
+
+    let bot = Bot::new(token).throttle(Limits::default());
+
+    let handler = dptree::entry()
+        .branch(
+            Update::filter_message()
+                .enter_dialogue::<Message, InMemStorage<State>, State>()
+                .branch(dptree::case![State::Send].endpoint(send))
+        )
+        .branch(
+            Update::filter_message()
+                .filter_command::<Command>()
+                .enter_dialogue::<Message, InMemStorage<State>, State>()
+                .endpoint(answer)
+        );
+
+    Dispatcher::builder(bot, handler)
+        .dependencies(dptree::deps![InMemStorage::<State>::new()])
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
 }
