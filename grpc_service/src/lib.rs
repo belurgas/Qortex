8ed51a6a73044fdb404d5de5231cae3687e991f0
Belurgas <70036882+belurgas@users.noreply.github.com>
2025-05-30 23:13:47 +0300

    Update lib.rs

--- grpc_service/src/lib.rs
+++ grpc_service/src/lib.rs
@@ -6,9 +6,12 @@ use hello_world::greeter_server::{Greeter, GreeterServer};
 use hello_world::{HelloReply, HelloRequest};
 use tonic::{Request, Response, Status};
 
+/// Struct for Greeter impl. For test view...
 #[derive(Debug, Default)]
 pub struct MyGreeter {}
 
+/// When the client send SayHello method gRPC server reply for his request
+/// The methos discrive in hellowrorld.proto
 #[tonic::async_trait]
 impl Greeter for MyGreeter {
     async fn say_hello(
@@ -17,6 +20,7 @@ impl Greeter for MyGreeter {
     ) -> Result<Response<HelloReply>, Status> {
         println!("Получен запрос: {:?}", request);
 
+        // Reply for request
         let reply = HelloReply {
             message: format!("Привет {}!", request.into_inner().name),
         };
