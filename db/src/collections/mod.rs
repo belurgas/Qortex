d69d3f2242a84487893c07b6ac5d2a8f8a62d1d9
Belurgas <70036882+belurgas@users.noreply.github.com>
2025-06-02 17:51:42 +0300

    From<Role> for Bson impl

--- db/src/collections/mod.rs
+++ db/src/collections/mod.rs
@@ -1,3 +1,13 @@
+use mongodb::bson::Bson;
+use user::Role;
+
 pub mod user;
 pub mod answer;
 pub mod history;
+
+// Convert Role to Bson string
+impl From<Role> for Bson {
+    fn from(role: Role) -> Self {
+        Bson::String(role.to_string())
+    }
+}
