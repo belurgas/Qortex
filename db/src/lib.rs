77d0b8151c66170687558709df3762aee2f3f3e8
Belurgas <70036882+belurgas@users.noreply.github.com>
2025-06-02 18:13:15 +0300

    Change to correct DB struct

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,205 +1,84 @@
 use std::{env, sync::{Arc, Mutex}};
 
-use anyhow::Context;
+use anyhow::{Context, Result};
 use chrono::{DateTime, Utc};
-use mongodb::{bson::{doc, Document, to_bson}, options::ClientOptions, Client, Collection, Database};
+use collections::{answer::AnswerRequest, history::UserHistory, user::User};
+use mongodb::{bson::{doc, oid::ObjectId, to_bson, Document}, options::ClientOptions, Client, Collection};
 use serde::{Deserialize, Serialize};
 
 pub mod collections;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct User {
-    pub telegram_id: i64,
-    pub username: Option<String>,
-    pub role: UserRole,
-    pub created_at: DateTime<Utc>,
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub enum StatusCode {
+    Exist,
+    ObjectIdError,
+    UserId(ObjectId),
+    GroupId(ObjectId),
 }
 
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub enum UserRole {
-    DEFAULT,
-    MODER,
-    ADMIN,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct HistoryMessage {
-    pub role: String,
-    pub content: String,
-    pub timestamp: DateTime<Utc>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct UserHistory {
-    pub telegram_id: i64,
-    pub messages: Vec<HistoryMessage>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug)]
-pub struct AnswerRequest {
-    pub telegram_id: i64,
-    pub text: String,
-    pub status: AnswerStatus,
-    pub timestamp: DateTime<Utc>,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
-pub enum AnswerStatus {
-    ACCEPTED,
-    REVIEWED,
-}
-
-struct DatabasePool {
+pub struct Database {
     client: Client,
-    db_name: String,
+    users_collection: Arc<Collection<User>>,
+    history_collection: Arc<Collection<UserHistory>>,
+    answers_collection: Arc<Collection<AnswerRequest>>,
 }
 
-impl DatabasePool {
-    pub async fn new(uri: &str, db_name: &str) -> anyhow::Result<Arc<Mutex<Self>>> {
-        let mut client_options = ClientOptions::parse(uri).await?;
-        client_options.app_name = Some("ai-bot".to_string());
-
+impl Database {
+    // Database init
+    pub async fn new(
+        uri: &str,
+        db_name: &str
+    ) -> Result<Arc<Self>> {
+        let client_options = ClientOptions::parse(uri).await?;
         let client = Client::with_options(client_options)?;
+        let database = client.database(db_name);
 
-        // Check connection
-        client.list_database_names().await
-            .expect("Не удалось подключиться к MongoDB");
-
-        Ok(Arc::new(Mutex::new(DatabasePool { client, db_name: db_name.to_string() })))
-    }
-
-    pub fn db(&self) -> Database {
-        self.client.database(&self.db_name)
-    }
-
-    pub fn users(&self) -> Collection<User> {
-        self.db().collection("users")
-    }
-
-    pub fn history(&self) -> Collection<UserHistory> {
-        self.db().collection("user_history")
-    }
-
-    pub fn answers(&self) -> Collection<AnswerRequest> {
-        self.db().collection("answers")
-    }
-}
-
-pub struct DatabaseService {
-    pool: Arc<Mutex<DatabasePool>>,
-}
-
-impl DatabaseService {
-    pub async fn new(uri: &str, db_name: &str) -> anyhow::Result<Self> {
-        let pool = DatabasePool::new(uri, db_name).await?;
-        Ok(Self {
-            pool
-        })
-    }
-
-    //1. Checking/Registration user
-    pub async fn check_or_register_user(&self, user_id: i64, username: Option<String>) -> anyhow::Result<()> {
-        let pool = self.pool.lock().unwrap();
-        let users = pool.users();
-
-        let created_at_bson = to_bson(&Utc::now()).unwrap();
-        let role = to_bson(&UserRole::DEFAULT).unwrap();
-
-        let filter = doc! { "telegram_id": user_id };
-        let update = doc! {
-            "$setOnInsert": {
-                "telegram_id": user_id,
-                "username": username,
-                "role": role,
-                "created_at": created_at_bson,
-            }
-        };
-
-        users.update_one(filter, update).upsert(true)
-            .await
-            .context("Ошибка при регистрации пользователя")?;
-
-        Ok(())
-    }
-
-    //2. Change user role
-    pub async fn update_user_role(&self, username: &str, new_role: UserRole) -> anyhow::Result<bool> {
-        let pool = self.pool.lock().unwrap();
-        let users = pool.users();
-        
-        let new_role = to_bson(&new_role)?;
-
-        let filter = doc! { "username": username };
-        let update = doc! { "$set": { "role": new_role } };
-        
-        let result = users.update_one(filter, update).await?;
-        Ok(result.modified_count > 0)
-    }
-
-    // 4. Clear history
-    pub async fn clear_user_history(&self, user_id: i64) -> anyhow::Result<bool> {
-        let pool = self.pool.lock().unwrap();
-        let history = pool.history();
-        
-        let filter = doc! { "telegram_id": user_id };
-        let result = history.delete_one(filter).await?;
-        Ok(result.deleted_count > 0)
-    }
-
-    //5. Add user history
-    pub async fn add_message_to_history(
-        &self,
-        user_id: i64,
-        role: &str,
-        content: &str,
-        is_system: bool
-    ) -> anyhow::Result<()> {
-        let pool = self.pool.lock().unwrap();
-        let history = pool.history();
-        
-        let message = HistoryMessage {
-            role: role.to_string(),
-            content: content.to_string(),
-            timestamp: Utc::now(),
-        };
-
-        let filter = doc! { "telegram_id": user_id };
-
-        if is_system {
-            // Create new history with system prompt
-            let new_history = UserHistory {
-                telegram_id: user_id,
-                messages: vec![message],
-            };
-
-            history.replace_one(filter, new_history).upsert(true)
-                .await
-                .context("Ошибка при добавлении системного промпта")?;
-        } else {
-            // Add mes in act hist
-            let update = doc! {
-                "$push": {
-                    "messages": to_bson(&message)?
-                }
-            };
-
-            history.update_one(filter, update)
-                .await
-                .context("Ошибка при добавлении сообщения в историю")?;
+        // Collections check
+        let collections = database.list_collection_names().await?;
+        if !collections.contains(&"users".to_string()) {
+            database.create_collection("users").await?;
         }
 
-        Ok(())
+        if !collections.contains(&"user_history".to_string()) {
+            database.create_collection("user_history").await?;
+        }
+
+        if !collections.contains(&"answers".to_string()) {
+            database.create_collection("answers").await?;
+        }
+
+        // Get collections
+        let users_collections = database.collection::<User>("users");
+        let user_history = database.collection::<UserHistory>("user_history");
+        let answers_collection = database.collection::<AnswerRequest>("answers");
+
+        Ok(
+            Arc::new( Self {
+                client,
+                users_collection: Arc::new(users_collections),
+                history_collection: Arc::new(user_history),
+                answers_collection: Arc::new(answers_collection),
+            })
+        )
     }
 
-    // Получение истории для ИИ
-    pub async fn get_ai_history(&self, user_id: i64) -> anyhow::Result<Vec<HistoryMessage>> {
-        let pool = self.pool.lock().unwrap();
-        let history = pool.history();
-        
-        let filter = doc! { "telegram_id": user_id };
-        let result = history.find_one(filter).await?;
-        
-        Ok(result.map(|h| h.messages).unwrap_or_default())
+    // Add user if he doesn't exist in DB
+    pub async fn add_user(
+        &self,
+        user: User
+    ) -> Result<StatusCode> {
+        let existing_user = self.users_collection.find_one(doc! { "telegram_id": user.telegram_id }).await?;
+        if existing_user.is_some() {
+            return Ok(StatusCode::Exist);
+        }
+
+        let result = self.users_collection.insert_one(user).await?;
+        if let Some(id) = result.inserted_id.as_object_id() {
+            println!("User created successfully with id: {}", id);
+            return Ok(StatusCode::UserId(id));
+        } else {
+            return Ok(StatusCode::ObjectIdError);
+        }
     }
 }
 
