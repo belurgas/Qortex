21fd84af171bfe0d16a6b17881789605caff0e5d
belurgas <tenpart@mail.ru>
2025-06-13 22:32:07 +0300

    Delete no need proto files

--- grpc_service/build.rs
+++ grpc_service/build.rs
@@ -3,9 +3,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         // .out_dir("src/")
         .compile_protos(
             &[
-                "proto/helloworld.proto",
-                "proto/test.proto",
-                "proto/prompt_service.proto",
                 "proto/ai_service.proto",
             ],
             &["proto/"],
