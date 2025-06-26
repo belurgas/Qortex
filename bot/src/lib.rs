6737fda37f3d45ec32b4ef0cb0fb476da6e72df2
Belurgas <morethenworking@outlook.com>
2025-06-26 21:50:27 +0300

    Change from MongoDB to PostgreSQL

--- bot/src/lib.rs
+++ bot/src/lib.rs
@@ -1,29 +1,33 @@
-use db::Database;
+use db_pg::UserRepository;
 use handlers::{commands::{command_handler, Commander}, messages};
-use logging::{log_info, logger::setup_logger};
+use logging::{log_error, log_info, logger::setup_logger};
 use dotenvy::dotenv;
 use state::State;
 use teloxide::{adaptors::{throttle::Limits}, dispatching::dialogue::InMemStorage, prelude::*};
 use types::MyBot;
 use std::{env, sync::Arc};
 
-mod keyboards;
+use crate::handlers::callback::{callback_handler, CallbackHandlerFactory};
+
+pub mod keyboards;
 mod handlers;
-mod state;
-mod types;
+pub mod state;
+pub mod types;
 
 pub struct TelegramBot {
     pub bot: MyBot,
     pub storage: Arc<InMemStorage<State>>,
-    pub db: Arc<Database>,
+    pub db: Arc<UserRepository>,
+    pub callback_handlers: Arc<CallbackHandlerFactory>,
 }
 
 impl TelegramBot {
     /// Create Bot Copy
-    pub async fn new(bot_token: String, db: Arc<Database>) -> Arc<Self> {
+    pub async fn new(bot_token: String, db: Arc<UserRepository>) -> Arc<Self> {
         let bot = Bot::new(bot_token).throttle(Limits::default());
         let storage = InMemStorage::<State>::new();
-        Arc::new(TelegramBot { bot, storage, db })
+        let callback_handlers = Arc::new(CallbackHandlerFactory::new());
+        Arc::new(TelegramBot { bot, storage, db, callback_handlers })
     }
 
     /// Bot Start
@@ -44,6 +48,12 @@ impl TelegramBot {
                             }
                         )
                     )
+            ).branch(
+                Update::filter_callback_query().enter_dialogue::<CallbackQuery, InMemStorage<State>, State>().endpoint(
+                    |bot: Arc<TelegramBot>, dialogue, q| async move {
+                        callback_handler(bot, dialogue, q).await
+                    }
+                )
             );
 
         // Dispatch builder and starter
@@ -65,14 +75,17 @@ pub async fn start() {
     setup_logger().expect("Не удалось настроить логгер");
 
     let token = env::var("TOKEN").expect("Ошибка при получение токена из .env");
-    let uri = env::var("MONGODB_URI").unwrap();
+    let url = env::var("DB_URL").unwrap();
 
 
     log_info!("Бот запущен...");
 
-    let db = db::Database::new(&uri, "ai_base").await.unwrap();
+    let repo = UserRepository::new(&url).await.unwrap();
+    if let Err(e) = repo.init_table().await {
+        log_error!("Ошибка иницализации таблицы: {}", e);
+    }
 
     // Bot init
-    let bot = TelegramBot::new(token, db).await;
+    let bot = TelegramBot::new(token, repo.into()).await;
     let _urn = bot.run().await;
 }
