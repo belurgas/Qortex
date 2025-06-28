b3094601a25f04f0b7c289a94ce1e8c1300acb14
Belurgas <morethenworking@outlook.com>
2025-06-28 16:59:22 +0300

    Add new row for State enum

--- bot/src/state.rs
+++ bot/src/state.rs
@@ -1,7 +1,13 @@
+use db_pg::Message;
+
 #[derive(Clone, Default, Debug)]
 pub enum State {
     #[default]
     OnWaiting,
     Send,
     WaitQuestion,
+    ViewingMessages {
+        messages: Vec<Message>,
+        current_page: usize,
+    }
 }
